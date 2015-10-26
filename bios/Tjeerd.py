1ac7a4248e55371e0f4b2fad2179cc1d13a08b1e
toekel <tjeerd.slokker@gmail.com>
2015-10-26 14:59:21 +0100

    Tjeerd
    Code controleren

--- bios/Tjeerd.py
+++ bios/Tjeerd.py
@@ -1,4 +1,30 @@
 __author__ = 'Tjeerd'
 
-nl = 'Dikke Neger Lul'
-print(nl)
+
+#CODE CONTROLEREN
+'''
+import csv
+
+kaartbestand = 'kaartcodes1.csv'
+
+def kaartje_controleren():
+    kaartcode = input('Wat is de code van uw kaartje?: ')
+    try:
+        f = open(kaartbestand, 'r')
+        reader = csv.DictReader(f, delimiter=';')
+        tjeerd = False
+        for row in reader:
+            if row['kaartnummer'] == str(kaartcode):
+                tjeerd = True
+            else:
+                tjeerd = False
+        if tjeerd == True:
+            print('Uw code klopt, veel plezier met de film')
+        elif tjeerd == False:
+            print('Uw code klopt niet')
+    finally:
+        f.close()
+
+kaartje_controleren()
+
+'''
