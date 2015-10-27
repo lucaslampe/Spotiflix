ab82fc211417f36866b2cf489d4f5b018909d985
Jeroendevr <halsteren@gmail.com>
2015-10-27 11:30:01 +0100

    Werkend spotify web player

--- bios/Jeroen.py
+++ bios/Jeroen.py
@@ -1,18 +1,18 @@
 __author__ = 'jeroendevries'
-#python module voor contact met spotify
+
+# python module voor contact met spotify
+
 import spotipy
-import pprint
+
 
 def spotify(titel):
     """
     :param titel:
     :return:Een URL met een link naar een muziek suggestie op basis van de titel
     """
-    spotify = spotipy.Spotify()
-    suggestie = spotify.search(q=titel, limit=1)
+    spot = spotipy.Spotify()
+    suggestie = spot.search(q=titel, limit=1)
 
     return suggestie['tracks']['items'][0]['album']['external_urls']
 
-
-
-print(spotify("SPY"))
+print(spotify("Conquest of Paradise"))
