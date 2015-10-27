5029eb53e352b6311307b86a5a29ee0343d7411a
lucaslampe <lucaslampe@gmail.com>
2015-10-27 11:57:48 +0100

    Update test

--- bios/test.py
+++ bios/test.py
@@ -1,9 +1,15 @@
 __author__ = 'Carlo'
 from urllib.request import urlopen
 import xml.etree.ElementTree as ET
-page = urlopen("http://www.filmtotaal.nl/api/filmsoptv.xml?apikey=6i1yqv1xyhumkosv7nk8s9au34e20iuq&dag=27-10-2015&sorteer=0")
+import time
+
+filmtotaal_api = '6i1yqv1xyhumkosv7nk8s9au34e20iuq'
+datum = time.strftime("%d-%m-%Y")
+
+page = urlopen("http://www.filmtotaal.nl/api/filmsoptv.xml?apikey="+filmtotaal_api+"&dag="+datum+"&sorteer=0")
 #contents = page.read()
+
 tree = ET.parse(page)
 root = tree.getroot()
-for film in root.iter('titel'):
+for film in root.iter('film'):
     print(film.text)
