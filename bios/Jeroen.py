8f817776a7b156d2788c41764039be01b4cb1897
Jeroendevr <halsteren@gmail.com>
2015-10-27 16:23:42 +0100

    For loop spotify

--- bios/Jeroen.py
+++ bios/Jeroen.py
@@ -5,14 +5,24 @@ __author__ = 'jeroendevries'
 import spotipy
 
 
-def spotify(titel):
+def spotify(titel, jaar):
     """
     :param titel:
     :return:Een URL met een link naar een muziek suggestie op basis van de titel
     """
+    jaar = str(jaar)
     spot = spotipy.Spotify()
-    suggestie = spot.search(q=titel, limit=1)
 
-    return suggestie['tracks']['items'][0]['album']['external_urls']['spotify']
+    suggestie1 = spot.search(q=titel + " " + jaar, limit=1)
+    suggestie2 = spot.search(q=titel + " " + 'soundtrack', limit=1)
+    suggestie3 = spot.search(q=titel + " " + 'soundtrack' + " " + jaar, limit=1)
+    try:
+      for x in range(0,3):
+          y = x
+          sug + str(y) = suggestie + y['tracks']['items'][0]['album']['external_urls']['spotify']
+    except:
+        print("Helaas geen nummer gevonden")
 
-print(spotify("Conquest of Paradise"))
+    return (sug1, sug2, sug3)
+for i in range(0,3):
+    print(spotify("Paradise", 2010)[i])
