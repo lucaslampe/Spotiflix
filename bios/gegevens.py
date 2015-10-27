fcb123db48fb4c2089268aa9e6a7a54594734c5a
Ferhat42 <ferhatucar@hotmail.nl>
2015-10-27 15:12:57 +0100

    GUI

---
+++ bios/gegevens.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from tkinter.messagebox import showinfo
+
+def antwoord(text):
+    if "@aanbieder.nl" in text:
+        showinfo(title='popup', message='U bent nu ingelogd als aanbieder ')
+    else:
+        showinfo(title='popup', message='U bent nu ingelogd als klant ')
+
+window = Tk()
+label = Label(window, text='Voer uw inlognaam in:', )
+label2 = Label(window, text='Voer uw wachtwoord in: ',)
+naam = Entry(window)
+wachtwoord = Entry(window)
+
+label.grid(row=0, sticky=E)
+label2.grid(row=1, sticky=E)
+
+naam.grid(row=0, column=1)
+wachtwoord.grid(row=1, column=1)
+
+button = Button(window, text='Voer in',
+command=(lambda: antwoord(naam.get())))
+button.grid(row=2)
+window.mainloop()
+
+
+
+aanbieders = {
+    'Man on fire': 'Harderwijk',
+    'Saving Private Ryan': 'Amersfoort',
+    'Scarface': 'Utrecht'
+}
+
+def antwoord():
+    showinfo(title='popup', message='Deze film kunt u kijken bij bioscoop ')
+
+
+window = Tk()
+label = Label(window, text='Films die vandaag beschikbaar zijn:')
+label.grid(row=0)
+
+knop = Checkbutton(window, text='Man on fire',command=antwoord)
+knop2= Checkbutton(window, text='Saving Private Ryan',command=antwoord)
+knop3= Checkbutton(window, text='Scarface', command=antwoord)
+
+knop.grid(row=1, sticky=W)
+knop2.grid(row=2, sticky=W)
+knop3.grid(row=3, sticky=W)
+
+
+window.mainloop()
+
+
+
+
