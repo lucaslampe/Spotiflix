d20b5d69dec5f3f3f6d69222d566aa9673876015
Jeroendevr <halsteren@gmail.com>
2015-10-27 17:47:21 +0100

    Titel, artiest en url

--- bios/Jeroen.py
+++ bios/Jeroen.py
@@ -5,24 +5,26 @@ __author__ = 'jeroendevries'
 import spotipy
 
 
-def spotify(titel, jaar):
+def spotify(titel):
     """
     :param titel:
-    :return:Een URL met een link naar een muziek suggestie op basis van de titel
+    :return:(URL,Titel nummer, Artiest)
     """
-    jaar = str(jaar)
     spot = spotipy.Spotify()
 
-    suggestie1 = spot.search(q=titel + " " + jaar, limit=1)
-    suggestie2 = spot.search(q=titel + " " + 'soundtrack', limit=1)
-    suggestie3 = spot.search(q=titel + " " + 'soundtrack' + " " + jaar, limit=1)
+    suggestie1 = spot.search(q=titel + " ", limit=1)
+    suggestie2 = spot.search(q=titel + " " + 'From', limit=1)
+
     try:
-      for x in range(0,3):
-          y = x
-          sug + str(y) = suggestie + y['tracks']['items'][0]['album']['external_urls']['spotify']
+        sug1 = (suggestie1['tracks']['items'][0]['external_urls']['spotify'], suggestie1['tracks']['items'][0]['name'], suggestie1['tracks']['items'][0]["artists"][0]['name'])
+        sug2 = (suggestie2['tracks']['items'][0]['external_urls']['spotify'], suggestie2['tracks']['items'][0]['name'], suggestie2['tracks']['items'][0]["artists"][0]['name'])
+        return (sug1, sug2)
     except:
         print("Helaas geen nummer gevonden")
+    return None
 
-    return (sug1, sug2, sug3)
-for i in range(0,3):
-    print(spotify("Paradise", 2010)[i])
+
+song = spotify("Conquest of paradise")
+if song is not None:
+    for i in range(0,2):
+        print(song[i])
